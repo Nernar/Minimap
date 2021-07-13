a9bcc3586f9b179a8cd9c57113b4570770817345
MaXFeeD <46795182+MaXFeeD@users.noreply.github.com>
2021-07-13 22:59:25 +0300

    Oh golly, I'd forgot about you

--- dev/header.js
+++ dev/header.js
@@ -1,21 +1,13 @@
 /*
-	 __  __ _	   _ __  __			 
-	|  \/  (_)	 (_)  \/  |			
-	| \  / |_ _ __  _| \  / | __ _ _ __  
-	| |\/| | | '_ \| | |\/| |/ _` | '_ \ 
-	| |  | | | | | | | |  | | (_| | |_) |
-	|_|  |_|_|_| |_|_|_|  |_|\__,_| .__/ 
-								  | |	
-								  |_|	
-   
+
    Copyright 2015 MxGoldo (twitter.com/MxGoldo)
-   Copyright 2020 Nernar (github.com/nernar)
+   Copyright 2020-2021 Nernar (github.com/nernar)
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
 
-	   http://www.apache.org/licenses/LICENSE-2.0
+       http://www.apache.org/licenses/LICENSE-2.0
 
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
--- launcher.js
+++ launcher.js
@@ -1,1 +1,24 @@
-Launch();
+const launchModification = function(additionalScope) {
+	if (additionalScope !== undefined) {
+		__mod__.RunMod(additionalScope);
+		return;
+	}
+	Launch();
+};
+
+(function() {
+	try {
+		ConfigureMultiplayer({
+			name: "auto",
+			version: "auto",
+			isClientOnly: true
+		});
+	} catch (e) {
+		launchModification({
+			isOutdated: true
+		});
+		return;
+	}
+
+	launchModification();
+})();
